ecef65b173298393646bf551dc6eb7f591774bd5
macak.nicolas@gmail.com <xmacakn@stuba.sk>
2023-03-02 10:34:40 +0100

    init

---
+++ TodoTutorial4/MauiProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoTutorial4;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
